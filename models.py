bd5a7201eabc988328760589d6436e311db80417
Carlos Sánchez <karlossanpa@gmail.com>
2020-05-17 17:51:22 +0200

    Migrate to PostgreSQL

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
-from djongo import models
+from django_better_admin_arrayfield.models.fields import ArrayField
+from django.db import models
 
 from howru_helpers import UTCTime
 
@@ -7,36 +8,55 @@ from howru_helpers import UTCTime
 class Patient(models.Model):
     identifier = models.CharField(primary_key=True, max_length=10)
     name = models.CharField(max_length=100, null=True)
-    #picture = models.ImageField()
+    # picture = models.ImageField()
     picture = models.CharField(max_length=100, null=True)
-    gender = models.CharField(max_length=1, null=True)
+    _gender = models.CharField(choices=[("M", "Male"), ("F", "Female"), ("O", "Other")], max_length=1,
+                               db_column="gender", null=True)
     language = models.CharField(choices=[("GB", "English"), ("ES", "Spanish")], max_length=2)
     username = models.CharField(max_length=20, null=True)
     _schedule = models.DateTimeField(
         db_column="schedule"
     )
+
     @property
     def schedule(self):
         return self._schedule
+
     @schedule.setter
     def schedule(self, value):
         utc_result = UTCTime.get_utc_result(value)
         self._schedule = utc_result
 
+    @property
+    def gender(self):
+        return self._gender
+
+    @gender.setter
+    def gender(self, value):
+        if value in ["Masculino", "Male"]:
+            gender = "M"
+        elif value in ["Femenino", "Female"]:
+            gender = "F"
+        else:
+            gender = "O"
+        self._gender = gender
+
 
 class Question(models.Model):
-    identifier = models.ObjectIdField()
-    responses = models.TextField()
+    responses = ArrayField(
+        models.CharField(max_length=50)
+    )
     text = models.CharField(max_length=100)
-    creator_id = models.CharField(max_length=100)
+    creator_id = models.ForeignKey(User, on_delete=models.PROTECT)
     public = models.BooleanField()
     language = models.CharField(choices=[("GB", "English"), ("ES", "Spanish")], max_length=2)
 
 
 class JournalEntry(models.Model):
-    question_id = models.GenericObjectIdField()
-    patient_id = models.CharField(max_length=10)
-    doctor_id = models.GenericObjectIdField()
+    question_id = models.ForeignKey(Question, on_delete=models.PROTECT)
+    patient_id = models.ForeignKey(Patient, on_delete=models.PROTECT)
+    doctor_id = models.ForeignKey(User, on_delete=models.PROTECT)
+
     class Meta:
         abstract = True
 
